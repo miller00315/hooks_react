b2e3dc189855b51ce0f91f5fb58ab5082766ad38
Miller Cesar de Oliveira <miller@academiadapele.com.br>
2021-05-30 19:02:01 -0300

    Utlizando useCallback

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
+import P from 'prop-types';
 import './App.css';
-import {useState} from 'react';
+import React, {useState, useCallback} from 'react';
+
+const Button = React.memo(({onClick}) => <button onClick={() => onClick(10)}>+</button>);
+
+Button.protoTypes = {
+  onClick: P.func,
+}
 
 function App() {
   const [counter, setCounter] = useState(0);
+
+  const incrementCounter = useCallback((num) => {
+    setCounter((oldCounter) => oldCounter + num);
+  }, []);
 
   return (
     <div className="App">
       <p>Teste 3</p>
       <h1>C1: {counter}</h1>
-      <button onClick={() => setCounter(counter + 1)}>+</button>
+      <Button onClick={incrementCounter} />
     </div>
   );
 }
